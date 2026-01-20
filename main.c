0b22884e53ff6ce3eee5dbb0dae16a17e0f0f2c6
Eike Friedrich <jeikefriedrich@gmail.com>
2026-01-20 11:01:12 +0000

    enable WDT
    enable watchdog at default 18ms
    CLRWDT seems to take 2 instructions cycles
    Update PWM timing and configuration settings

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
  * beta release
  * PIC12F675 Code Workspace
  * 
- * each loop (pwm step) is ~30us independent of anything else
+ * each loop (pwm step) is ~32us independent of anything else
  * PWM_STEPS = length of half a duty cycle
  * frequency = 1/(2*PWM_STEPS*.000030)
  * increasing PWM_STEPS = slower frequency, but smoother transition
  * the smaller lookup table is only really useful to save space (on flash)
  * 
  * increase from 127 to 152Hz with xc8 optimisation set in project properties
- * 127Hz with 128 steps
+ * 122Hz with 128 steps
  * 157Hz with 100 steps
- * 244Hz 64 steps
+ * 241Hz 64 steps
  * 256Hz 60 steps
- * 300Hz 50 steps
+ * 311Hz 50 steps
  * measured OEM supply as 50V p2p, 242Hz
  */
 
@@ -23,7 +23,7 @@
 
 // CONFIG
 #pragma config FOSC = INTRCIO
-#pragma config WDTE = OFF
+#pragma config WDTE = ON
 #pragma config PWRTE = ON
 #pragma config MCLRE = ON
 #pragma config BOREN = ON
@@ -72,21 +72,28 @@ uint8_t phase = 0;      // 0 = A, 1 = B
 uint8_t adc_state = 0;  // 0=idle, 1=converting
 
 void setup() {
+    // Calibrate oscillator
+    //asm("call 0x348C"); //523Hz
+    //asm("call 0b10000000");
+    //asm("movwf OSCCAL");
+    // memory address 3fff goes back to 3444
+    //OSCCAL = 0b11111100; // max value
+    //OSCCAL = 0b10000000; //0x80 centre value
+    //OSCCAL = 0b00000000; //min value
+    
     // Analogue channel setup
     //ANSEL = 0b00010100;     // 2us per sample (2x11) + 11.5 + 0.5 = 34us
     ANSEL = 0b01010100;   // 4us per sample = (4*11) + 11.5 +0.5 = 56us
     //        -|||---- bits 6:4 ADC conversion clock 101=4us 001=2us
     //        ----|||| bits 3:0 set AN3:0 channels as analogue
-    // since we are not wating for ADC to complete 4us is better bet at 3V
+    // since we are not waiting for ADC to complete 4us is better bet at 3V
     
     // GP4 & GP5 outputs
     TRISIO = 0b00001100;         // all outputs except AN2 GP3(MCLRE))
-    
     //ADC configuration
     ADCON0 = 0b00001001;         // left justified, Vdd ref, AN2, ADC ON
     // Disable comparator
-    CMCON = 0b00000111;         // Comparator Off (Lowest power)
-    
+    CMCON = 0b00000111;          // Comparator Off (Lowest power)
     // Initialise outputs
     GPIO = 0x00;
 }
@@ -98,6 +105,7 @@ void main(void) {
            // Start ADC conversion (non-blocking)
            ADCON0bits.GO_nDONE = 1;
            adc_state = 1;
+           CLRWDT();    // reset watchdog timer, default 18ms timeout
        }
 
        // Check if ADC is done (runs every cycle, minimal overhead)
